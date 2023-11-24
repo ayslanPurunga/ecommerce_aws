fe5da6d3a8b6dd82396262fa670644917ecf491d
Ayslan Fernandes <lalanfernandes@gmail.com>
2023-11-24 14:42:50 -0300

    criando 1° func lambda

---
+++ lib/productsApp-stack.ts
@@ -0,0 +1,28 @@
+import * as lambda from "aws-cdk-lib/aws-lambda";
+
+import * as  lambdaNodejs from "aws-cdk-lib/aws-lambda-nodejs";
+
+import * as cdk from "aws-cdk-lib";
+
+import { Construct } from "constructs";
+
+export class ProductsAppStack extends cdk.Stack {
+    readonly productsFetchHandler: lambdaNodejs.NodejsFunction
+
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props)
+
+        this.productsFetchHandler = new lambdaNodejs.NodejsFunction(this, "ProductsFetchFunction", {
+            runtime: lambda.Runtime.NODEJS_16_X,
+            functionName: "ProductsFetchFunction",
+            entry: "lambda/products/productsFetchFunction.ts",
+            handler: "handler",
+            memorySize: 128,
+            timeout: cdk.Duration.seconds(5),
+            bundling: {
+                minify: true,
+                sourceMap: false
+            }
+        })
+    }
+} 
