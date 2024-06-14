467a01cd654698296df04ab8e8c004e27d936121
Ayslan Fernandes <lalanfernandes@gmail.com>
2024-06-14 13:50:01 -0300

    aws s3 e websockets

--- bin/e_commerce_aws.ts
+++ bin/e_commerce_aws.ts
@@ -7,6 +7,8 @@ import { ProductsAppLayersStack } from '../lib/productsAppLayers-stack';
 import { EventsDdbStack } from '../lib/eventsDdb-stack';
 import { OrdersAppLayersStack } from '../lib/ordersAppLayers-stack';
 import { OrdersAppStack } from '../lib/ordersApp-stack';
+import { InvoiceWSApiStack } from '../lib/invoiceWSApi-stack'
+import { InvoicesAppLayersStack } from '../lib/invoicesAppLayers-stack';
 
 const app = new cdk.App();
 
@@ -63,3 +65,19 @@ const eCommerceApiStack = new ECommerceApiStack(app, "ECommerceApi", {
 })
 eCommerceApiStack.addDependency(productsAppStack)
 eCommerceApiStack.addDependency(ordersAppStack)
+
+const invoicesAppLayersStack = new InvoicesAppLayersStack(app, "InvoicesAppLayer", {
+  tags: {
+    cost: "InvoiceApp",
+    team: "SiecolaCode"
+  },
+  env: env
+})
+const invoiceWSApiStack = new InvoiceWSApiStack(app, "InvoiceApi", {
+  tags: {
+    cost: "Invoice",
+    team: "SiecolaCode"
+  },
+  env: env
+})
+invoiceWSApiStack.addDependency(invoicesAppLayersStack)
